47207c1c2ee3e43eac5bf9b65bcbc958a78fca06
John Britti <jakebritti@gmail.com>
2016-09-27 23:24:16 -0400

    +got markov sort of working

--- markov-test.py
+++ markov-test.py
@@ -2,31 +2,68 @@ import re
 import random
 
 
-def markov(text, gram, start, steps):
-    out = ""
-    reg = re.compile('\n')
-    words = reg.sub('', text).split(' ')
-    reg = re.compile('[.?!]')
-    for i in range(len(words)):
-        m = reg.match(words[i])
-        if m is not None:
-            words[i] = reg.sub('', words[i])
-            words.insert(i + 1, m.group())
-    print(words[0:5])
-    return out
+def markov(text, start, steps):
+    out = list(start)
+    words = re.sub(r'\n', ' ', text).split(' ')
+    # add_list = []
+    # for i in range(len(words)):
+    #     m = re.search(r'[.,/#!$%\^&*;:{}=\-_`~()"]', words[i])
+    #     if m is not None:
+    #         print(words[i])
+    #         words[i] = re.sub(r'[.,/#!$%\^&*;:{}=\-_`~()"]', '', words[i])
+    #         add_list.append((i + 1, m.group()))
+    # for elem in add_list:
+    #     words.insert(elem[0], elem[1])
+    # print(words[0:100])
+    gram = start
+    for i in range(steps):
+        out += [next_word(words, gram)]
+        gram = [out[i - len(start)] for i in range(len(start))]
+    return ' '.join(out)
 
 
-def next_word(text, words):
-    return random.choice([text[i[1] + 1] for i in find_sublist(text, words)])
+def next_word(text, gram):
+    # print(gram)
+    if len(gram) == 0:
+        print("Word not found, going full random.")
+        return random.choice(text)
+    next_list = [text[i[1] + 1] for i in find_sublist(text, gram) if i[1] + 1 < len(text)]
+    if len(next_list) < 1:
+        return next_word(text, gram[1:])
+    else:
+        return random.choice(next_list)
 
 
 def find_sublist(l, sl):
     out = []
+    # for i in range(len(l)):
+    #
     sll = len(sl)
     for ind in (i for i, e in enumerate(l) if e == sl[0]):
-        if list[ind:ind+sll]:
+        if l[ind:ind+sll] == sl:
             out.append((ind, ind+sll-1))
     return out
+run = True
+inp = ''
+txt = ''
+while run:
+    inp = input('Input file: ')
+    if inp.lower() == 'q':
+        run = False
+        break
+    else:
+        try:
+            file = open(inp, 'r', encoding="utf8")
+            txt = ''.join([i for i in file])
+            break
+        except FileNotFoundError:
+            print("File not found. Try again or (q)uit.")
 
-markov('''
-''', 2, '', 10)
+
+while run:
+    inp = input('Input starting strings separated by spaces (ex:"He tried") or (q)uit: ')
+    if inp.lower() == 'q':
+        run = False
+        break
+    else:
+        print("--> " + markov(txt, inp.split(' '), 20))
